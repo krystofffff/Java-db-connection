5a733ced8a1d36e97d8bcaebceb2637fc68f75ea
Krystof Krmaschek <krystof2508@seznam.cz>
2022-09-03 16:24:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "admin");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from people");
+
+            while (resultSet.next()){
+                System.out.print(resultSet.getInt("id") + " ");
+                System.out.println(resultSet.getString("firstName"));
+
+            }
+
+
+
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+}
